a4c5330bb64d8daa4272b27931f5581ed7a27619
Tdiasdacosta <timeodiasda@gmail.com>
2025-11-18 19:04:11 -0500

    Ajout de la structure fonctionnelle Vuejs : index.html, script.js, data.js, style.css

--- data.js
+++ data.js
@@ -1,1 +1,18 @@
-404: Not Found
+// Tableau d’objets exemple
+var items = [
+  {
+    title: "Chat",
+    definition: "Un animal domestique apprécié pour sa compagnie.",
+    image: "https://placekitten.com/200/200"
+  },
+  {
+    title: "Chien",
+    definition: "Un fidèle compagnon réputé pour son intelligence.",
+    image: "https://placedog.net/200/200"
+  },
+  {
+    title: "Lapin",
+    definition: "Petit animal herbivore aux grandes oreilles.",
+    image: "https://placekitten.com/201/201"
+  }
+];
--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-404: Not Found
+new Vue({
+  el: '#app',
+  data: {
+    index: 0,
+    showDef: false,
+    items: window.items // référence globale
+  },
+  methods: {
+    nextItem() {
+      this.index = (this.index + 1) % this.items.length;
+      this.showDef = false;
+    },
+    prevItem() {
+      this.index = (this.index - 1 + this.items.length) % this.items.length;
+      this.showDef = false;
+    },
+    toggleShow() {
+      this.showDef = !this.showDef;
+    }
+  },
+  template: `
+    <div class="viewer">
+      <img :src="items[index].image" :alt="items[index].title" class="img" />
+      <h2 v-if="!showDef">{{items[index].title}}</h2>
+      <p v-else>{{items[index].definition}}</p>
+      <button @click="toggleShow">
+        {{ showDef ? "Affiche le titre" : "Affiche la définition" }}
+      </button>
+      <div class="nav">
+        <button @click="prevItem">&lt; Précédent</button>
+        <button @click="nextItem">Suivant &gt;</button>
+      </div>
+    </div>
+  `
+});
