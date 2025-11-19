92b7e11cffdd94f9f7639c37db95f4a33cd62793
Tdiasdacosta <timeodiasda@gmail.com>
2025-11-19 16:35:58 -0500

    Update image sources to higher resolution URLs

--- data.js
+++ data.js
@@ -3,16 +3,16 @@ const items = [
   {
     id: 1,
     title: "Image 1",
-    src: "https://picsum.photos/200?random=1"
+    src: "https://picsum.photos/300?random=1"
   },
   {
     id: 2,
     title: "Image 2",
-    src: "https://picsum.photos/200?random=2"
+    src: "https://picsum.photos/300?random=2"
   },
   {
     id: 3,
     title: "Image 3",
-    src: "https://picsum.photos/200?random=3"
+    src: "https://picsum.photos/300?random=3"
   }
 ];
